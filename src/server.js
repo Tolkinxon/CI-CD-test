92c24431e505802cbadc4c35ce1669a5a5ec9e82
Tolkinxon <krosstony639@gmail.com>
2025-09-05 00:07:52 +0500

    Test

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const { PORT } = serverConfig;
 
 const app = express();
 
-app.get('/hello', (req, res)=>{res.send("Hello world")});
+app.get('/hello', (req, res)=>{res.send("Hello Tolkinxon conguratulations")});
 app.listen(PORT, ()=>console.log(`Server runnig on PORT ${PORT}`));
